d489ef772b06db86034b52a5250ee36777f095aa
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-29 14:59:51 +0300

    fixed merge conflicts

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,35 +1,5 @@
 from ..models import User
 from flask_wtf import FlaskForm
-<<<<<<< HEAD
-from wtforms.validators import Required, Email, EqualTo, ValidationError
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-
-
-class LoginForm(FlaskForm):
-    email= StringField('Email',validators = [Required(), Email()])
-    password = PasswordField("Password", validators = [Required()])
-    remember = BooleanField('Remember me')
-    submit = SubmitField("Login")
-
-def validate_email(form,data_field):
-        if User.query.filter_by(email = data_field.data).first():
-            raise ValidationError('There is an account with that email')
-
-def validate_username(form,data_field):
-        if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That username is taken')
-
-class RegisterForm(FlaskForm):
-    username = StringField("Username", validators=[Required(), validate_username])
-    email = StringField("Email", validators=[Email(), Required(), validate_email])
-    password = PasswordField('Password', validators =[Required(),
-    EqualTo("password_confirm", message="Password must match")])
-    password_confirm = PasswordField('Confirm Password', validators = [Required()])
-
-    submit = SubmitField('Sign Up')
-
-    
-=======
 from wtforms import StringField, PasswordField, SubmitField, ValidationError, BooleanField
 from wtforms.validators import Required, Email, EqualTo, Length
 
@@ -63,4 +33,3 @@ class NewPassword(FlaskForm):
     password = PasswordField('Password',validators=[Required()], render_kw={"placeholder": "Password"})
     password_repeat = PasswordField('Repeat Password', validators=[Required(),EqualTo('password')], render_kw={"placeholder": "Confirm Password"})
     submit = SubmitField('Change Password')
->>>>>>> b3a4641111124f9321b173e15e0cbef1545eba88
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-from flask import render_template, redirect, request, url_for, flash
-from flask_login import UserMixin, current_user, login_user, logout_user
-from .import auth
-from ..import db
-from ..models import User
-from .forms import LoginForm, RegisterForm
-
-
-@auth.route('/login',methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-
-    title = "Login"
-    Login = LoginForm()
-
-    if Login.validate_on_submit:
-        user = User.query.filter_by(email = Login.email.data).first()
-        if user is not None and user.verify_password(Login.password.data):
-            login_user(user, Login.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid, Try again')
-
-    return render_template('auth/login.html', Login = Login, title=title)
-
-
-
-@auth.route('/register', methods=['GET', 'POST'])
-def register():
-    title= "Register Account"
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        user = User(username = form.username.data, email = form.email.data, password = form.password.data, is_admin=False)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-
-    return render_template('auth/register.html', title=title, form= form)
-
-
- 
-@auth.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('main.index'))
-=======
 from flask import render_template, redirect, url_for, flash, request
 from ..models import User
 from flask_login import login_user, login_required, logout_user, current_user
@@ -126,4 +78,3 @@ def new_password(token):
         flash('Your password has been reset')
         return redirect(url_for('auth.login'))
     return render_template('auth/change_password.html',title='Reset Password',form=form)
->>>>>>> b3a4641111124f9321b173e15e0cbef1545eba88
